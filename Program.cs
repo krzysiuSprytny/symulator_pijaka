7b53a33d7a593448b0d023906225dc39fe928562
Krzysztof <pada_deszcz123@wp.pl>
2025-03-20 09:20:12 +0100

    3/5

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ class Program{
         int LiczbaProb = int.Parse(Console.ReadLine());
         Random random = new Random();
         int[] PozycjeKońcowe = new int[LiczbaKrokow];
+        Dictionary<int, int> Statystyki = new Dictionary<int, int>();
         for(int i = 0; i < LiczbaProb; i++){
             int Pozycja = 0;
             int[] Kroki = new int[LiczbaKrokow];
@@ -17,9 +18,15 @@ class Program{
                 Kroki[j] = Pozycja;
             }
             PozycjeKońcowe[i] = Pozycja;
+            if(Statystyki.ContainsKey(Pozycja))Statystyki[Pozycja]++;
+            else Statystyki[Pozycja] = 1;
             Console.WriteLine($"Proba {i + 1}: {string.Join(", ", Kroki)}");
         }
         Console.WriteLine("\nPozycje koncowe po kazdej probie: ");
         Console.WriteLine(string.Join(", ", PozycjeKońcowe));
+        Console.WriteLine("\nPodsumowanie wystapien poszczegolnych pozycji koncowych: ");
+        foreach (var kvp in Statystyki){
+            Console.WriteLine($"Pozycja {kvp.Key}: {kvp.Value} razy");
+        }
     }
 }
