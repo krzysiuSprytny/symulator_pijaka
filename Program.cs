5b8e6cfcf79dbc0d25d52c4d3b112471fbce4940
Krzysztof <pada_deszcz123@wp.pl>
2025-03-20 08:40:36 +0100

    1/5

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System; 
+
+class Program{
+    static void Main(){
+        Console.Write("Podaj liczbe krokow w jednej probie: ");
+        int LiczbaKrokow = int.Parse(Console.ReadLine());
+        Console.Write("Podaj liczbe prob do zasymulowania: ");
+        int LiczbaProb = int.Parse(Console.ReadLine());
+        Random random = new Random();
+        for(int i = 0; i < LiczbaProb; i++){
+            int Pozycja = 0;
+            int[] Kroki = new int[LiczbaKrokow];
+            for(int j = 0; j < LiczbaKrokow; j++){
+                int krok = random.Next(2) == 0 ? -1 : 1;
+                Pozycja += krok;
+                Kroki[j] = Pozycja;
+            }
+            Console.WriteLine($"Proba {i + 1}: {string.Join(", ", Kroki)}");
+        }
+    }
+}
